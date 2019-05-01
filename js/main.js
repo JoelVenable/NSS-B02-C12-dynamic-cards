5549c7feb2f03d98fec8bc37f93dc9fdc99002f1
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-05-01 15:21:42 -0500

    eslint corrections

--- js/main.js
+++ js/main.js
@@ -1,35 +1,33 @@
 let iterator = 0;
 
-
 function clickButton() {
   const div = document.querySelector("#card-container"),
-    newDiv = document.createElement('div'),
-    input = document.querySelector('input'),
-    p = document.createElement('p'),
-    btn = document.createElement('button');
+    newDiv = document.createElement("div"),
+    input = document.querySelector("input"),
+    p = document.createElement("p"),
+    btn = document.createElement("button");
 
-    newDiv.appendChild(p);
-    newDiv.classList = "card"
-    p.textContent = input.value;
-    input.value = "";
-    newDiv.appendChild(btn);
-    newDiv.id = `container-${iterator}`;
-    btn.id = `button-${iterator}`;
-    btn.textContent = "Delete"
-    div.appendChild(newDiv);
-  
+  newDiv.appendChild(p);
+  newDiv.classList = "card";
+  p.textContent = input.value;
+  input.value = "";
+  newDiv.appendChild(btn);
+  newDiv.id = `container-${iterator}`;
+  btn.id = `button-${iterator}`;
+  btn.textContent = "Delete";
+  div.appendChild(newDiv);
 
-    iterator++;
+  iterator++;
 }
 
-document.querySelector('#create').addEventListener("click",clickButton);
+document.querySelector("#create").addEventListener("click", clickButton);
 
-document.querySelector("#card-container").addEventListener("click",(event => {
+document.querySelector("#card-container").addEventListener("click", event => {
+  console.log(event.srcElement.id.slice(7));
   let buttonId = event.srcElement.id.slice(7),
-    eventTarget = event.srcElement.localName.toLowerCase()
+    eventTarget = event.srcElement.localName.toLowerCase();
 
   if (eventTarget === "button") {
     document.querySelector(`#container-${buttonId}`).remove();
   }
-
-}))
+});
